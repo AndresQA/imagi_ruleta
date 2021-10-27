880b2cd8996f92c61e168a4b78baa08cf306adbf
AndresQA <34174822+AndresQA@users.noreply.github.com>
2021-10-27 10:12:12 -0500

    roulette styles

--- src/pages/Final/Final.tsx
+++ src/pages/Final/Final.tsx
@@ -5,7 +5,15 @@ const Final = () => {
     return <div className="Index backgroundType2 backgroundImg" style={{ backgroundImage: 'url(/images/background3.svg)' }}>
 
         <div className="container">
-            <img className="logo" src="/images/logo.png" alt="logo" />
+            {switch (key) {
+                case value:
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+            }
         </div>
 
     </div>
